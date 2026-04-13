944b02c4a2ef44ac376a34238f51947b98e4dd6a
Gome1 <b123698526@gmail.com>
2026-04-13 22:50:44 +0800

    bug test

--- GeneralNods/HitBox/hit_box.gd
+++ GeneralNods/HitBox/hit_box.gd
@@ -1,11 +1,11 @@
 class_name HitBox extends Area2D # 傷害判定盒，繼承自 Area2D
+
 signal Damaged(damage : int) # 受擊時對外廣播傷害值
 
 func _ready() -> void: # 初始化時暫未處理
 	pass # 佔位以便後續補邏輯
 
-func _process( _delta: float) -> void: # 每幀處理暫未使用
-	pass # 佔位以便擴展
+
 
 func TakeDamage(damage : int ) -> void: # 接收外部傳入的傷害
 	print( "TakeDamage:", damage  ) # 列印傷害數值方便除錯
