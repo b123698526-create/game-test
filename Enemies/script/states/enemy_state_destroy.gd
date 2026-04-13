442488f6fe2253bfae03c821b0145c9c44803f14
Gome1 <b123698526@gmail.com>
2026-04-13 18:24:41 +0800

    bug

---
+++ Enemies/script/states/enemy_state_destroy.gd
@@ -0,0 +1,51 @@
+class_name EnemyStateDestroy extends EnemyState
+
+@export var anim_name : String = "destroy"
+@export var knockback_speed : float = 200.0
+@export var decelerate_speed : float = 10.0
+
+@export_category("AI")
+
+
+var _direction : Vector2
+var _animation_finished : bool = false
+
+
+func init() -> void:
+	enemy.enemy_damage.connect( _on_enemy_damaged )
+
+	
+func _ready() -> void:# 基類初始化暫不處理
+	pass # 佔位以便子類覆寫
+
+func enter() -> void:# 狀態進入時的掛鉤
+	enemy.invulnerable = true
+	_animation_finished = false
+	_direction = enemy.global_position.direction_to(enemy.player.global_position)
+	#捕捉PLAYER反向量
+	enemy.velocity = _direction * -knockback_speed
+	enemy.set_direction(_direction)
+	
+	enemy.update_animation(anim_name)
+	enemy.animation_player.animation_finished.connect(_on_animation_finished)	
+
+func exit() -> void: # 狀態退出時的掛鉤
+	enemy.animation_player.animation_finished.disconnect(_on_animation_finished)
+	enemy.invulnerable = false
+
+
+func process(_delta : float) -> EnemyState:# 每幀更新，可返回要切換的狀態
+	if _animation_finished == true :
+		return next_state
+	
+	enemy.velocity = enemy.velocity * decelerate_speed * _delta
+	return null# 預設不切換狀態
+
+func physics(_delta : float) -> EnemyState:# 物理幀更新，可返回要切換的狀態
+	return null# 預設不切換狀態
+	
+func _on_enemy_damaged() -> void:
+	state_machine.change_state( self )
+
+func _on_animation_finished( _a : String ) -> void :
+	_animation_finished = true 
