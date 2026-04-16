ada38c56cc1e9d42bed414f323e68aac6751a2ef
Gome1 <b123698526@gmail.com>
2026-04-16 23:18:56 +0800

    4/16 bug 2

--- Player/scripts/state.gd
+++ Player/scripts/state.gd
@@ -3,19 +3,22 @@ class_name State extends Node # 狀態基類，繼承 Node
 var player: Player # 玩家引用，供子類訪問
 
 func _ready() -> void:# 基類初始化暫不處理
-	pass # 佔位以便子類覆寫
+	pass 
+
+func _init() -> void:
+	pass
 
 func enter() -> void:# 狀態進入時的掛鉤
-	pass# 留空供子類實現
+	pass
 
 func exit() -> void: # 狀態退出時的掛鉤
-	pass# 留空供子類實現
+	pass
 
 func process(_delta : float) -> State:# 每幀更新，可返回要切換的狀態
-	return null# 預設不切換狀態
+	return null
 
 func physics(_delta : float) -> State:# 物理幀更新，可返回要切換的狀態
-	return null# 預設不切換狀態
+	return null
 	
 func handle_input( _event : InputEvent ) -> State:# 輸入事件處理，可返回要切換的狀態
-	return null# 預設不處理輸入
+	return null
